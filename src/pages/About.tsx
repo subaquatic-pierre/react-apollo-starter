ce8cb050cdf49a53f7ea8525683a2f0d8cb46d55
Pierre <subaquatic.pierre@gmail.com>
2021-04-26 10:03:54 +0400

    Test the pipeline is restarted on git push

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const About: React.FC = () => {
-  return <div className="page">About Us!</div>;
+  return (
+    <div className="page">
+      <h2>About Us!</h2>
+      <p>Make sure the repo update is working in the pipeline</p>
+    </div>
+  );
 };
